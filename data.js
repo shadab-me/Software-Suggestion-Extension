8e80cd11846f5aa7cd48b8a49e2c1d446503c9f4
Shadab Ali <shadabali604@gmail.com>
2020-09-27 11:04:00 +0530

    added: update for new extension

---
+++ data.js
@@ -0,0 +1,125 @@
+let list = [
+    {
+    name : "WordPress – The most popular website building platform",
+    description: "WP is a blogging platform developed in PHP language and it supports building your website on your own server with PHP/MySQL database. As one of the best web development tools, the software can be used as CMS (Content Management System) to set up a commercial website. Its features include: the WYSIWYG text editor, co-authoring with multiple authors, permalinks optimized for search engines, static page, Trackback/Pingback, member registration/login,etc.",
+    download: "https://wordpress.com/"
+    },
+    {
+    name: "Mockplus – An all-in-one web prototyping tool",
+    description: "As an all-inclusive design tool for designers and developers, Mockplus can be used to create faster, smarter and easier website prototypes with simple drag-and-drop, supporting mobile and desktop apps prototyping too. With a singular mission to keep users focused on design itself rather than the tool, the software has offered a number of incomparable features to make better design and development.",
+    Download: "https://www.mockplus.com/web-prototyping-tool"
+    },
+    
+    {
+    name: "Macaw – The best web design software for code-savvy people",
+    description: "Out of the best website design tools, Macaw sets itself apart by providing the same flexibility as your favorite image editor but also writes semantic HTML and CSS. Also, it gives the possibility to make a responsive webpage with beautiful typography and system fonts, letting you style the elements at once. If you are looking for free website design software with a moderate learning curve and flexibility, then don’t miss out the software.",
+    Download: "http://macaw.co/"
+    },
+    
+    {
+    name: "Weebly – The easiest web builder among the newbies and professional designers",
+    description: "As one of the most popular website development tools among people who want to start their own sites, Weebly is very easy-to-use and aimed at personal, business and professional websites. Above all, it enables you to make site-building work without any coding experience, meanwhile you can choose to edit the HTML/CSS if you have the coding background.",
+    Download: "https://www.weebly.com"
+    },
+    
+    {
+    name: "Adobe Dreamweaver – One of the most trustworthy web developer tools",
+    description: "This is a proprietary web development tool from Adobe Systems, available for both macOS and Windows. Besides, it provides a nice environment for you to work on the web, which combines with a code editor along with a live view. Another cool thing is the software offers free trial, which gives you some time to experiment and see whether it’s right for you.",
+    Download: "https://www.adobe.com/sea/products/dreamweaver.html"
+    },
+    
+    {
+    name: "Bootstrap - The most popular HTML, CSS, and JS library in the world",
+    description: "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS-based and JavaScript-based design templates for typography, forms, buttons, navigation, and other interface components.As a popular and powerful framework, Bootstrap is designed for front-end developers to make beautiful designs for websites with HTML, CSS, and JS. With the most advanced development features, Boostrap is well-received by discerning technical users around the world.",
+    Download: "https://getbootstrap.com"
+    },
+    
+    {
+    name: "Codepen - The best online code editor and open-source learning environment",
+    description: "CodePen is a web development environment to build, test, and discover front-end code to learn and debug. It's a friendly development environment for front-end designers and developers to find design patterns and inspiration.",
+    Download: "https://codepen.io"
+    },
+    
+    {
+    name: "MAMP - A local web development solution for macOS and Windows",
+    description: "MAMP is a good open-source integration software that can be installed in macOS and Windows. It provides three main products, including MAMP Pro 5, MAMAP, and NAMO. MAMP is a free local server, MAMP Pro 5 is a commercial, professional frontend utility that enables Mac OS X users to install Apache server, MySQL, PHP, accelerator and PHPMyAdmin with ease.",
+    Download: "https://www.mamp.info/en/windows/"
+    },
+    
+    {
+    name: "Github - The world's leading software development platform",
+    description: "If you don’t know GitHub, you are not a truly qualified developer. As a development platform, it connects over 40 million developers from all over the world and provides inspiration for them to find the best solutions, high-quality suggestions, and open sources. Also, it offers services such as code review, project management, and bug tracking for every project.",
+    Download: "https://github.com/"
+    },
+    
+    {
+    name: "Photoshop - Best photo, image, and design editing software",
+    description: "Because Photoshop is an essential web development tool these days, more people are being enticed to master this software. With its powerful features, it has become an integral part of website building.",
+    Download:"https://www.photoshop.com"
+    },
+    
+    {
+    name: "AngularJS - Excellent front-end JavaScript framework",
+    description: "AngularJS is an excellent front-end JS framework that is used in many Google products. It is a library written in JavaScript, which can be added to an HTML page via the <script> tag. It has lots of useful features, such as MVC (Model–view–controller), modular, automated two-way data binding, semantic tagging, and dependency injection. ",
+    Download: "https://angularjs.org"
+    },
+    
+    {
+    name: "MySQL - One of the world’s most popular open-source databases",
+    description: "Developed by MySQL AB of Sweden and currently owned by Oracle, MySQL is one of the most popular relational database management systems today. It’s the best free and open source Relational Database Management System (RDBMS) for web applications.",
+    Download: "https://www.mysql.com"
+    },
+    
+    {
+    name: "Sass - most reliable, mature, and robust CSS extension language",
+    description: "Sass is the most reliable, mature, and robust CSS extension language. This tool helps to extend the functionality of an existing CSS of a site like variables, inheritance, and nesting with ease.",
+    Download: "http://sass-lang.com/"
+    },
+    
+    {
+    name: "Sublime Text -  cross-platform source code editor",
+    description: "Sublime Text is a proprietary cross-platform source code editor. This app development tool natively supports many programming languages and markup languages.",
+    Download:  "https://www.sublimetext.com/"
+    },
+    
+    {
+    name: "Chrome Developer Tools - debugging tools built into Chrome",
+    description: "The Chrome Developer Tools are a set of debugging tools built into Chrome. These tools allow developers to do wide varieties of testing which easily saved lots of time.",
+    Download:  "https://developer.chrome.com/devtools"
+    },
+    
+    {
+    name: "Atom - open-source hackable desktop code editor",
+    description: "Atom is an open-source hackable desktop code editor from GitHub. Use this tool to access thousands of open source packages, explore text editor themes built by the Atom community, and work in CSS, HTML, and JavaScript. You can also run Teletype for Atom to share workspaces for synchronous code editing.",
+    Download: "https://atom.io"
+    },
+    
+    {
+    name: "CodeKit - front-end web development tool",
+    description: "Codekit is a front-end web development tool. This tool provides support to build websites faster. It combines, minifies and syntax-checks JavaScript. It also optimizes images.",
+    Download:  "https://codekitapp.com"
+    },
+    
+    {
+    name: "Visual Code Studio - code editor developed by Microsoft",
+    description: "Visual Studio Code is a source-code editor developed by Microsoft which works on Windows, Linux and macOS platforms. VCS notable features are debugging, intelligent code completion, snippets, embedded Git control and GitHub, syntax highlighting, and code refactoring.",
+    Download: "https://code.visualstudio.com"
+    },
+    
+    {
+    name: "ReactJS - most popular Javascript application framework",
+    description: "React is probably the most popular Javascript application framework right now. It is actively created and maintained by Facebook, and was an early proponent of component based UI development. The other big reason React is quite popular is because of React Native, which allows you to use the same style of application code to create Native UI Android and iOS applications.",
+    Download: "https://reactjs.org/"
+    },
+    
+    {
+    name: "Figma - world's leading UI design software",
+    description: "Figma is currently the world's leading UI design software. It’s gained popularity mainly due to its collaboration features, allowing multiple people work on the same document simultaneously.",
+    Download: "https://www.figma.com/"
+    }
+    
+]
+    
+    
+    
+    
---
+++ main.js
@@ -0,0 +1,9 @@
+function RandomSoftware(arr){
+   console.log(Math.floor(Math.random() * list.length)+1);
+}
+RandomSoftware();
+
+
+list.forEach((item) => {
+   console.log(item.name)
+})
